46ff26328104ebaa256103986581311b1a2a261f
chc1129 <chc1129@gmail.com>
2020-10-15 23:57:51 +0900

    python prg fix

--- 10/fib.py
+++ 10/fib.py
@@ -1,3 +1,5 @@
+import os
+import time
 import sys
 
 def fibonacci(n):
@@ -7,9 +9,24 @@ def fibonacci(n):
 	else:
 		return a
 
+def elapsed_time(f):
+	def wrapper(*args, **kwargs):
+		st = time.time()
+		v = f(*args, **kwargs)
+		print(f"{f.__name__}: {time.time() - st}")
+		return v
+	return wrapper
+
+@elapsed_time
+def get_sequential(nums):
+	for num in nums:
+		print(fibonacci(num))
+
 def main():
 	n = int(sys.argv[1])
-	print(fibonacci(n))
+	# 返される値は環境で異なる
+	nums = [n] * os.cpu_count()
+	get_sequential(nums)
 
 if __name__ == "__main__":
 	main()
