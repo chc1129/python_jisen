b0a9d152d2f5600f3553d6798aa8872f661ca51b
chc1129 <chc1129@gmail.com>
2020-10-16 20:33:19 +0900

    python prg fix

--- 10/fib.py
+++ 10/fib.py
@@ -1,32 +1,44 @@
 import os
 import time
 import sys
+from concurrent.futures import (
+    ProcessPoolExecutor,
+    as_completed
+)
 
 def fibonacci(n):
-	a, b = 0, 1
-	for _ in range(n):
-		a, b = b, b + a
-	else:
-		return a
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, b + a
+    else:
+        return a
 
 def elapsed_time(f):
-	def wrapper(*args, **kwargs):
-		st = time.time()
-		v = f(*args, **kwargs)
-		print(f"{f.__name__}: {time.time() - st}")
-		return v
-	return wrapper
+    def wrapper(*args, **kwargs):
+        st = time.time()
+        v = f(*args, **kwargs)
+        print(f"{f.__name__}: {time.time() - st}")
+        return v
+    return wrapper
 
 @elapsed_time
 def get_sequential(nums):
-	for num in nums:
-		print(fibonacci(num))
+    for num in nums:
+        print(fibonacci(num))
+
+@elapsed_time
+def get_multi_process(nums):
+    with ProcessPoolExecutor() as e:
+        futures = [e.submit(fibonacci, num)
+                    for num in nums]
+        for future in as_completed(futures):
+            print(future.result())
 
 def main():
-	n = int(sys.argv[1])
-	# 返される値は環境で異なる
-	nums = [n] * os.cpu_count()
-	get_sequential(nums)
+    n = int(sys.argv[1])
+    # 返される値は環境で異なる
+    nums = [n] * os.cpu_count()
+    get_multi_process(nums)
 
 if __name__ == "__main__":
-	main()
+    main()
