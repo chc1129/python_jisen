6bd0384d37201e503845ed35371f98ef639dde87
chc1129 <chc1129@gmail.com>
2020-10-17 06:41:39 +0900

    python prg fix

---
+++ 12/booksearch_module.py
@@ -0,0 +1,11 @@
+import unittest
+
+# アプリケーションコード
+def booksearch():
+	# 任意の処理
+	return {}
+
+class BookSearchTest(unittest.TestCase):
+	# booksearch()のテストコード
+	def test_booksearch(self):
+		self.assertEqual({}, booksearch())
